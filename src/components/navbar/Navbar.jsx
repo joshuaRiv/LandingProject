06fdc9e1afd4f11ac43fb4df5611c50ab9a4c4ff
joshuaRiv <brayanrivera10@gmail.com>
2024-04-16 21:19:04 -0600

    Navbar menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../logo.svg';
+import logo from '../../assets/aicraftLogo.png';
 import './navbar.css';
 
 const Navbar = () => {
@@ -8,21 +8,23 @@ const Navbar = () => {
 
   return (
     <div className="gpt3__navbar">
-      <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo">
-          <img src={logo} />
-        </div>
-        <div className="gpt3__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
-        </div>
+      {/* <div className="gpt3__navbar-links"> */}
+      <div className="gpt3__navbar-links_logo">
+        <img src={logo} />
       </div>
+      {/* </div> */}
+      <div className="gpt3__navbar-links_container">
+        <p><a href="#home">Servicios</a></p>
+        <p><a href="#wgpt3">Productos</a></p>
+        <p><a href="#possibility">Entrenamiento</a></p>
+        <p><a href="#features">Recursos</a></p>
+        <p><a href="#blog">Contacto</a></p>
+        <p><a href="#blog">Nosotros</a></p>
+      </div>
+
       <div className="gpt3__navbar-sign">
-        <p>Sign in</p>
-        <button type="button">Sign up</button>
+        {/* <p>Sign in</p> */}
+        <button type="button">Inicia Sesión</button>
       </div>
       <div className="gpt3__navbar-menu">
         {toggleMenu
@@ -31,15 +33,16 @@ const Navbar = () => {
         {toggleMenu && (
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
-            <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
-            <p><a href="#features">Case Studies</a></p>
-            <p><a href="#blog">Library</a></p>
+            <p><a href="#home">Servicios</a></p>
+            <p><a href="#wgpt3">Productos</a></p>
+            <p><a href="#possibility">Entrenamiento</a></p>
+            <p><a href="#features">Recursos</a></p>
+            <p><a href="#blog">Contacto</a></p>
+            <p><a href="#blog">Nosotros</a></p>
           </div>
           <div className="gpt3__navbar-menu_container-links-sign">
-            <p>Sign in</p>
-            <button type="button">Sign up</button>
+            {/* <p>Sign in</p> */}
+            <button type="button">Inicia Sesión</button>
           </div>
         </div>
         )}
