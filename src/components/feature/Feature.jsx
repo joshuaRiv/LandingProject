b278e3bc1f486e584795800d706ab27b165929ff
joshuaRiv <brayanrivera10@gmail.com>
2024-05-07 12:47:58 -0600

    Features section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = ({ title, text }) => (
+const Feature = ({ title }) => (
   <div className="gpt3__features-container__feature">
-    <div className="gpt3__features-container__feature-title">
-      <div />
-      <h1>{title}</h1>
-    </div>
-    <div className="gpt3__features-container_feature-text">
-      <p>{text}</p>
-    </div>
+    <a href="...">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+    </a>
   </div>
 );
 
--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,19 +4,19 @@ import './features.css';
 
 const featuresData = [
   {
-    title: 'Improving end distrusts instantly',
+    title: 'Consultoría',
     text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
   },
   {
-    title: 'Become the tended active',
+    title: 'Auditoría',
     text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.',
   },
   {
-    title: 'Message or am nothing',
+    title: 'Política e IA responsable',
     text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.',
   },
   {
-    title: 'Really boy law county',
+    title: 'Educación y entrenamiento',
     text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush..',
   },
 ];
@@ -33,8 +33,16 @@ const Features = () => (
     </div>
     <div className="gpt3__features-container">
       {featuresData.map((item, index) => (
-        <Feature title={item.title} text={item.text} key={item.title + index} />
+        <Feature title={item.title} key={item.title + index} />
       ))}
+    </div>
+    <div className="featureButtons">
+      <div className="featuresButtonContainerAi">
+        <button type="button" className="featureBtns">Porqué Aicraft</button>
+      </div>
+      <div className="featuresButtonContainerPr">
+        <button type="button" className="featureBtns">Posibles proyectos</button>
+      </div>
     </div>
   </div>
 );
